ba8232cc0a072b44f724264ffdc97ec3876921d3
Marinov Radoslav (LHM-LE) <Radoslav.Marinov@liebherr.com>
2017-10-06 17:23:37 +0300

    Add send post request with file read from the file system

---
+++ upload-file-from-browser-to-server/server.js
@@ -0,0 +1,21 @@
+var http = require("http")
+var express = require("express")
+var url = require("url")
+var fs = require("fs")
+
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+});
+
+app.listen(80);
+console.log('Server is listening ... ')
