194ccdcf13105bdd3e065ab5240fda773bc40c2a
Radoslav <rikotech@abv.bg>
2018-01-03 11:15:19 +0200

    Apply working state

--- node/express-angular-app/dist/js/main.js
+++ node/express-angular-app/dist/js/main.js
@@ -39,8 +39,9 @@ angular.module("myApp").controller("postController", function($scope, $http) {
 	}
 })
  
-angular.module("myApp").controller("myController", function($scope) {
+angular.module("myApp").controller("myController", function($scope, $compile, $element, $http) {
 	$scope.ButtonStatus  = "OFF";
+	$scope.inputValue = "Button"
 	$scope.myStyle = buttonGreen  //{'background-color':'#00FF00', 'font-size': '24px', 'width': '80px'}
 	$scope.toggleRelay = function(){
 		if($scope.ButtonStatus == "OFF")
@@ -53,5 +54,34 @@ angular.module("myApp").controller("myController", function($scope) {
 			$scope.ButtonStatus = "OFF";
 			$scope.myStyle = buttonGreen;	
 		}
+	}
+	$scope.changeText = function(id) {
+		var myElement = angular.element(document.querySelector('#'+id));
+		
+		var input = "<input type='txt' ng-model='inputValue'></input>"
+		var button = "<button class='save' ng-click='saveChanges()'>Save</button>"
+		var linkFn = $compile(input);
+		var inputEl = linkFn($scope);
+		linkFn = $compile(button);
+		var buttEl = linkFn($scope);
+
+
+		myElement.append(inputEl)
+		myElement.append(buttEl)
+	}
+	$scope.saveChanges = function() {
+		console.log($('span', $element).attr('id'))
+		var spanId = $('span', $element).attr('id');
+		$('span', $element).text($scope.inputValue);
+		$http.post("/html-params.json", {id: spanId, name: $scope.inputValue}, {headers: {'Content-Type': 'application/json'}}).then(
+			function(res) { //Success
+				console.log(res.data)
+			},
+			function(res) {	// Failed
+
+			}
+		)
+		$('input', $element).remove()
+		$('.save', $element).remove()
 	}
 })
--- node/express-angular-app/main.js
+++ node/express-angular-app/main.js
@@ -4,19 +4,35 @@ var bodyParser = require('body-parser')
 //== JSDOM
 //requeres file system
 var fs = require('fs')
-var htmlSource = fs.readFileSync("public/html/index.html", "utf8")
+var htmlSource = fs.readFileSync("dist/html/index.html", "utf8")
+
+
+// Set upt JSDOM HTML JQUERY-parser
 var jsdom = require('jsdom');
 const {JSDOM} = jsdom;
 const dom = new JSDOM(htmlSource);
 var q = dom.window.document.querySelector.bind(dom.window.document);
-q("title").innerHTML = "FUCKITTT";
-q("title").setAttribute('riko-Men', '6')
-htmlSource = dom.window.document.querySelector("html").outerHTML
-var serialized = dom.serialize()
-console.log(serialized)
+// q("title").innerHTML = "FUCKITTT";
+// q("title").setAttribute('riko-Men', '6')
 
 
-fs.writeFileSync("public/html/index2.html", '<!DOCTYPE html>\n' + serialized, "utf8");
+//Extract and parse JSON customizer file
+var jsonSource;
+var htmlCust;
+var jsonFileObj
+function setHtmlFormJson(){
+	jsonSource = fs.readFileSync("public/js/regular/html-customizer.JSON", "utf8")
+	htmlCust = JSON.parse(jsonSource);
+	jsonFileObj = htmlCust.pages.home.objects;
+	for (var i = 0; i < jsonFileObj.length; i++) {
+		console.log(jsonFileObj[i]);
+		q("#" + jsonFileObj[i].id).innerHTML = jsonFileObj[i].text
+	}
+	var serialized = dom.serialize()
+	console.log(serialized)
+	fs.writeFileSync("dist/html/index.html", serialized, "utf8");
+}
+
 
 
 
@@ -33,13 +49,26 @@ app.use( bodyParser.json() );
 app.use(express.static(__dirname + "/dist"))
 
 app.get('/', function(req, res){
-	console.log("Request URL: "  + req.url)
+	setHtmlFormJson();
+	console.log("Request URL: root"  + req.url)
 	res.sendFile(__dirname + "/dist/html/index.html");
 });
 
 app.post("/html-params.json", function(req, res) {
+	var obj = req.body;
+
+
+	for(var i = 0; i < jsonFileObj.length; i++) {
+		if( jsonFileObj[i].id === obj.id ) {
+			jsonFileObj[i].text = obj.name;
+			console.log("Object ID: " + obj.id);
+			break;
+		}
+	}
+
+	fs.writeFileSync("public/js/regular/html-customizer.JSON", JSON.stringify(htmlCust, null, 2) ,"utf8")
 	console.log("Request URL: " + req.url + ",   " + "Request body: " + JSON.stringify(req.body) );
 	res.send("Post Accepted!")
 })
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
